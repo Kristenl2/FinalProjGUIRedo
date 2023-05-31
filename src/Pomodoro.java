45ad349a46b18969b4a3b55629599c89e369c045
student <ivyl72@nycstudents.net>
2023-05-31 10:57:59 -0400

    5/31 morning

--- src/Pomodoro.java
+++ src/Pomodoro.java
@@ -15,10 +15,19 @@ public class Pomodoro extends JFrame implements ActionListener {
     private JPanel bottom;
 
     private int seconds;
+    private Timer time;
 
     public Pomodoro(){
         setup();
-       // createUIComponents();
+        time = new Timer(1000, null);
+        staticTimerLabel = new JLabel("Time Remaining");
+        dynamicTimerLabel = new JLabel();
+        // createUIComponents();
+
+
+        this.time_label = new JLabel(hour_string + ":" + min_string + ":" + sec_string);
+        this.time_label.setBounds(175,100,100,100);
+        this.add(this.time_label);
     }
 
     private void setup(){
@@ -33,6 +42,7 @@ public class Pomodoro extends JFrame implements ActionListener {
         shortBreak.addActionListener(this);
         longBreak.addActionListener(this);
         pause.addActionListener(this);
+        time.addActionListener(this);
     }
 
     public void actionPerformed(ActionEvent ea){
@@ -41,9 +51,19 @@ public class Pomodoro extends JFrame implements ActionListener {
             JButton button = (JButton)source;
             String name = button.getText();
 
-            if(name.equals(pomodoro)){
-
+            if (name.equals("Pomodoro")) {
+                seconds = 1500;
+                timerFires();
+            } else if (name.equals("Short Break")){
+                seconds = 300;
+                timerFires();
+            } else if (name.equals("Long Break")) {
+                seconds = 600;
+                timerFires();
             }
+
+        } else if (source instanceof Timer) {
+
         }
     }
 
