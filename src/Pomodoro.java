5053c69e003cb09f0f29f57a82a33c75232029be
Kristen <klee1751@bths.edu>
2023-05-31 00:12:55 -0400

    5/30

--- src/Pomodoro.java
+++ src/Pomodoro.java
@@ -1,2 +1,56 @@
-package PACKAGE_NAME;public class Pomodoro {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Pomodoro extends JFrame implements ActionListener {
+    private JButton pomodoro;
+    private JButton shortBreak;
+    private JButton longBreak;
+    private JButton pause;
+    private JPanel screen;
+    private JPanel top;
+    private JPanel middle;
+    private JLabel countdown;
+    private JPanel bottom;
+
+    private int seconds;
+
+    public Pomodoro(){
+        setup();
+       // createUIComponents();
+    }
+
+    private void setup(){
+        setContentPane(screen);
+        setTitle("PomodoroTimer");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400,320);
+        setLocation(500,250);
+        setVisible(true);
+
+        pomodoro.addActionListener(this);
+        shortBreak.addActionListener(this);
+        longBreak.addActionListener(this);
+        pause.addActionListener(this);
+    }
+
+    public void actionPerformed(ActionEvent ea){
+        Object source = ea.getSource();
+        if(source instanceof JButton){
+            JButton button = (JButton)source;
+            String name = button.getText();
+
+            if(name.equals(pomodoro)){
+
+            }
+        }
+    }
+
+
+
+//    private void createUIComponents(){
+//        setContentPane(screen);
+//        setTitle("PomodoroTimer");
+//    }
 }
